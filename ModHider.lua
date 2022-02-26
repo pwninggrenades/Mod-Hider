bfff944fba33f95133d84585ae78894302e839ba
pwn <88950549+pwninggrenades@users.noreply.github.com>
2022-02-26 17:50:08 +0000

    Add files via upload

---
+++ ModHider.lua
@@ -0,0 +1,11 @@
+function MenuCallbackHandler:build_mods_list()
+    return {}
+end
+
+function MenuCallbackHandler:is_modded_client()
+    return false
+end
+ 
+function MenuCallbackHandler:is_not_modded_client()
+    return true
+end
